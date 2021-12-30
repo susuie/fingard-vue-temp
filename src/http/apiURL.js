1f72ce83186d4f97023f99f8fca0bf64575fe73f
susie <susie8711@163.com>
2021-12-30 12:49:52 +0800

    feat: 去掉多余文件

--- src/http/apiURL.js
+++
@@ -1,7 +0,0 @@
-//let baseURL = 'rbttest.fingard.cn'
-//let webURL = `http://${baseURL}/manage`
-let webURL = "/manage";
-// let wsURL = `ws://${baseURL}/manage`
-// let reconURL = `http://${baseURL}/recon`
-
-export { webURL };
--- src/views/404.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p>哎呀~你访问的页面找不到了</p>
-</template>
